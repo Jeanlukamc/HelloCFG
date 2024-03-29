0c4c5ce415152d94dd8951b6a739a45d9886489b
Jeanlukamc <102693379+Jeanlukamc@users.noreply.github.com>
2024-03-29 15:09:01 -0600

    Added input
    Still needs attributes

--- test_html.py
+++ test_html.py
@@ -34,7 +34,8 @@ def test_input_tokenizer( file, tokens ):
 cnf_html_dict = dictionary_creation( html.chomsky_normal_form( ).productions( ) )
 @pytest.mark.parametrize( "dictionary, file, test_type, result", [( cnf_html_dict, "FILES\\HTML_Files\\HTML_2.txt", "Normal Tag Test", True ),
                                                                   ( cnf_html_dict, "FILES\\HTML_Files\\HTML_3.txt", "Head and Body Test", True ),
-                                                                  ( cnf_html_dict, "FILES\\HTML_Files\\HTML_4.txt", "Heading 1 - 6 Test", True )] )
+                                                                  ( cnf_html_dict, "FILES\\HTML_Files\\HTML_4.txt", "Heading 1 - 6 Test", True ),
+                                                                  ( cnf_html_dict, "FILES\\HTML_Files\\HTML_5.txt", "Line Break and Horizontal Rule Test", True )] )
 def test_CYK_XML( dictionary, file, test_type, result ):
     """Tests the input strings to make sure they are valid for the language"""
     file_input = input_collector( file )
