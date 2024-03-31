9e892f027b0f1e63c9a6506fa26aa4ebfb40d279
Jeanlukamc <102693379+Jeanlukamc@users.noreply.github.com>
2024-03-30 22:03:38 -0600

    Preparing tests for parsing

--- test_morse.py
+++ test_morse.py
@@ -7,8 +7,9 @@ from nltk import CFG
 from CYK_Parser import dictionary_creation, cyk_parser
 from morse_to_language import english_equivalent, morse_english_input_collector
 from langauge_to_morse import morse_code_equivalent, edit_morse_file_equivalent, morse_tokenizer
+from Grammars import morse_grammar
 
-@pytest.mark.parametrize( "file, string", [( "FILES\\Morse_Code_Files\\english_1.txt", '<WHILE> ( ALPHA <SUB> BETA <EQUALS><EQUALS> 15 )' ) ] )
+@pytest.mark.parametrize( "file, string", [( "FILES\\Morse_Code_Files\\english_1.txt", 'WELCOME TO THE 1ST TEST' ) ] )
 def test_input_collector( file, string ):
     """Tests that we get the proper input to parse"""
     file_input = morse_english_input_collector( file )
@@ -17,7 +18,7 @@ def test_input_collector( file, string ):
     print( f"Test #1 ( Valid Input Collection Test ) | File: [{file}] -> Expected Result: {string} | Result: {file_input == string} | PASSED")
     print( "--------------------------------------------------------------------------------------------------------------------------------------\n" )
 
-@pytest.mark.parametrize( "file, string", [( "FILES\\Morse_Code_Files\\english_1.txt", '<WHILE> ( ALPHA <SUB> BETA <EQUALS><EQUALS> 15 )' ) ] )
+@pytest.mark.parametrize( "file, string", [( "FILES\\Morse_Code_Files\\english_1.txt", 'WELCOME TO THE 1ST TEST' ) ] )
 def test_language_to_morse_to_language_translation( file, string ):
     """Tests that the conversion from english to morse makes sense when translated back"""
     file_input = morse_english_input_collector( file )
@@ -29,7 +30,7 @@ def test_language_to_morse_to_language_translation( file, string ):
     print( f"Test #2 ( English -> Morse -> English ) | File: [{file}] -> Expected Result: {string} | Result: {english_sentence == string} | PASSED")
     print( "--------------------------------------------------------------------------------------------------------------------------------------\n" )
 
-@pytest.mark.parametrize( "file_1, file_2, string", [( "FILES\\Morse_Code_Files\\english_1.txt", "FILES\\Morse_Code_Files\\morse_1.txt", "<#.--#....#..#.-..#.#>#/(#/.-#.-..#.--.#....#.-#/<#...#..-#-...#>#/-...#.#-#.-#/<#.#--.-#..-#.-#.-..#...#>#<#.#--.-#..-#.-#.-..#...#>#/.----#.....#/)#/" ) ] )
+@pytest.mark.parametrize( "file_1, file_2, string", [( "FILES\\Morse_Code_Files\\english_1.txt", "FILES\\Morse_Code_Files\\morse_1.txt", ".--#.#.-..#-.-.#---#--#.#/-#---#/-#....#.#/.----#...#-#/-#.#...#-#/" ) ] )
 def test_make_morse_file_equivalent( file_1, file_2, string ):
     """Tests that we have created an equivalent morse file from the english version"""
     my_input = morse_english_input_collector( file_1 )
@@ -40,3 +41,17 @@ def test_make_morse_file_equivalent( file_1, file_2, string ):
     assert( my_input == string )
     print( f"Test #3 ( Morse File Equivalent Maker Test ) | File: [{file_1}] -> File To Write: {file_2} | Result: {my_input == string} | PASSED")
     print( "--------------------------------------------------------------------------------------------------------------------------------------\n" )
+
+
+cnf_morse_dict = dictionary_creation( morse_grammar.chomsky_normal_form( ).productions( ) )
+@pytest.mark.parametrize( "dictionary, file, test_type, result",  [ ( cnf_morse_dict, "FILES\\Morse_Code_Files\\morse_1.txt", "Alphabet and Letters Test", True ) ] )
+def test_CYK_XML( dictionary, file, test_type, result ):
+    """Tests the input strings to make sure they are valid for the language"""
+
+    file_input = morse_english_input_collector( file )
+    tokens = morse_tokenizer( file_input )
+
+    assert ( cyk_parser( dictionary, tokens ) == result )
+    print( f"Test #4 ( CYK PARSING Morse Code ) | Test Type: [{test_type}] | Result: {result} | PASSED")
+    print( "--------------------------------------------------------------------------------------------------------------------------------------------------------------\n" )
+    
